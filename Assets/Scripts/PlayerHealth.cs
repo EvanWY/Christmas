9d7b8694037b6edfa4e0837c75157b0361076935
yxwang0603 <yxwang0603@gmail.com>
2016-12-17 13:28:54 -0500

    health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    private SpriteRenderer sRenderer;
+    public float initialHealth;
+    public float damage;
+    private float currentHealth;
+	// Use this for initialization
+	void Start () {
+        currentHealth = initialHealth;
+        sRenderer = GetComponentInChildren<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Obstacle"))
+        {
+            currentHealth -= damage;
+            Debug.Log("Collide with house, cause damage");
+        }
+    }
+}
