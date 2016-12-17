6f96840075eb1fdb4e93be344e3cb59bca2cc4d7
yang wang <yang wang>
2016-12-17 17:11:57 -0500

    menu

---
+++ Assets/OnClickLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnClickLoadScene : MonoBehaviour {
+
+	public string sceneName;
+
+	void Awake() {
+		GetComponent<Button>().onClick.AddListener(() => {
+			SceneManager.LoadScene(sceneName);
+		});
+	}
+}
