fa3b110029b4e9ce15d446f93e2fac3cb84775e1
yxwang0603 <yxwang0603@gmail.com>
2016-12-17 19:58:00 -0500

    added background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour {
+
+    public float offsetSpeed;
+    private SpriteRenderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * offsetSpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+	}
+}
