60427e1d570f0ffa3ef9aaa6ebfadac8a3166b28
Peilin Li <lipeilin@gatech.edu>
2016-12-17 23:04:46 -0500

    Combo system and UI added

---
+++ Assets/ComboManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//Manages the combo system and UI
+public class ComboManager : MonoBehaviour {
+	[Header("Variables to be configured")]
+	[SerializeField]
+	AudioClip[] comboUpSounds;
+	[SerializeField]
+	AudioClip comboBreakSound;
+	[SerializeField]
+	Text comboUI;
+
+	/// <summary>
+	/// The current combo number.
+	/// </summary>
+	public static int currentComboNum;
+	static AudioSource aus;
+	static Animator anim_UI;
+	static Text comboUI_static;
+	static AudioClip[] comboUpSounds_static;
+	static AudioClip comboBreakSound_static;
+
+	/// <summary>
+	/// Call this method to increase the current combo by 1.
+	/// </summary>
+	public static void ComboUp(){
+		currentComboNum += 1;
+		AudioPlay.PlaySound (aus, comboUpSounds_static [0]);
+		comboUI_static.text = currentComboNum.ToString ();
+		comboUI_static.enabled = true;
+		anim_UI.SetTrigger ("Up");
+		//to do: play different clips based on current combo Num;
+	}
+
+	/// <summary>
+	/// Call this method to break the combo.
+	/// </summary>
+	public static void ComboBreak(){
+		currentComboNum = 0;
+		AudioPlay.PlaySound (aus, comboBreakSound_static);
+		comboUI_static.text = currentComboNum.ToString ();
+		comboUI_static.enabled = false;
+		anim_UI.SetTrigger ("Break");
+	}
+
+	// Use this for initialization
+	void Awake () {
+		aus = GetComponent<AudioSource> ();
+		comboUpSounds_static = comboUpSounds;
+		comboBreakSound_static = comboBreakSound;
+		comboUI_static = comboUI; 
+		anim_UI = comboUI_static.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Q)){
+			ComboManager.ComboUp ();
+		}
+		else if(Input.GetKeyDown(KeyCode.W)){
+			ComboManager.ComboBreak ();
+		}
+	}
+}
---
+++ Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlay : MonoBehaviour
+{
+
+	public static void PlaySound(AudioSource _source, AudioClip _sound)
+	{
+		if (_source == null)
+			throw new UnityException ("Source is null");
+
+		if (_sound == null)
+			throw new UnityException ("Sound is null");
+
+		_source.Stop ();
+		_source.clip = _sound;
+		_source.Play ();
+
+	}
+
+	public static void PlayRandomSound(AudioSource _source, AudioClip[] _sounds)
+	{
+		if (_source == null)
+			throw new UnityException ("Source is null");
+
+		if (_sounds == null)
+			throw new UnityException ("Sounds is null");
+
+		if (_sounds.Length == 0)
+			throw new UnityException ("Sounds length is 0");
+
+
+		int randomNum = UnityEngine.Random.Range(0, _sounds.Length);
+
+		AudioClip _sound = _sounds[randomNum];
+			
+		_source.Stop();
+		_source.clip = _sound;
+		_source.Play();
+	}
+}
