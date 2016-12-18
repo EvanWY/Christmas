2f42352408e60929c1e2ea33cfa195fcf7540b1f
Peilin Li <lipeilin@gatech.edu>
2016-12-17 23:55:16 -0500

    combo behavior fixed. For testing, press Q to combo up; press W to break combo. As for how to use the combo manager, please refer to the comments of combo manager script.

--- Assets/Scripts/ComboManager.cs
+++ Assets/Scripts/ComboManager.cs
@@ -18,19 +18,29 @@ public class ComboManager : MonoBehaviour {
 	/// The current combo number.
 	/// </summary>
 	public static int currentComboNum;
+
+
+	static int comboNum_Display;
 	static AudioSource aus;
 	static Animator anim_UI;
 	static Text comboUI_static;
 	static AudioClip[] comboUpSounds_static;
 	static AudioClip comboBreakSound_static;
+	static bool reset;
 
 	/// <summary>
 	/// Call this method to increase the current combo by 1.
 	/// </summary>
 	public static void ComboUp(){
+		if (reset) {
+			reset = false;
+			comboNum_Display = 0;
+		}
+			
+		comboNum_Display += 1;
 		currentComboNum += 1;
 		AudioPlay.PlaySound (aus, comboUpSounds_static [0]);
-		comboUI_static.text = currentComboNum.ToString ();
+		comboUI_static.text = comboNum_Display.ToString ();
 		comboUI_static.enabled = true;
 		anim_UI.SetTrigger ("Up");
 		//to do: play different clips based on current combo Num;
@@ -40,11 +50,13 @@ public class ComboManager : MonoBehaviour {
 	/// Call this method to break the combo.
 	/// </summary>
 	public static void ComboBreak(){
+		if (comboNum_Display > 0) {
+			anim_UI.SetTrigger ("Break");
+			AudioPlay.PlaySound (aus, comboBreakSound_static);
+		}
+			
 		currentComboNum = 0;
-		AudioPlay.PlaySound (aus, comboBreakSound_static);
-		comboUI_static.text = currentComboNum.ToString ();
-		comboUI_static.enabled = false;
-		anim_UI.SetTrigger ("Break");
+		reset = true;
 	}
 
 	// Use this for initialization
@@ -53,16 +65,23 @@ public class ComboManager : MonoBehaviour {
 		comboUpSounds_static = comboUpSounds;
 		comboBreakSound_static = comboBreakSound;
 		comboUI_static = comboUI; 
+		comboUI.enabled = false;
 		anim_UI = comboUI_static.GetComponent<Animator> ();
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
+		Debug.Log ("Combo UI status: " + comboUI.enabled);
 		if(Input.GetKeyDown(KeyCode.Q)){
 			ComboManager.ComboUp ();
 		}
 		else if(Input.GetKeyDown(KeyCode.W)){
 			ComboManager.ComboBreak ();
 		}
+		else if(Input.GetKeyDown(KeyCode.A)){
+			Debug.Log ("Current Combo: " + ComboManager.currentComboNum);
+		}
+
 	}
 }
