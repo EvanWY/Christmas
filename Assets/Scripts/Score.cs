98319dad8c6088a10e8c309349edfd560bc28c7f
yxwang0603 <yxwang0603@gmail.com>
2016-12-18 15:09:24 -0500

    score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public int timeScore;
+    public int totalScore;
+    public int giftScore;
+    public int giftNum;
+
+    private float gameTime;
+    private bool manuallyPaused;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameTime = Time.time;
+        timeScore = Mathf.RoundToInt(10 * Mathf.Pow(gameTime, 1.2f));
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gift"))
+        {
+            giftNum = ComboManager.currentComboNum;
+            giftScore = Mathf.RoundToInt(Mathf.Pow(50, 1 + (0.05f) * giftNum));
+            totalScore = timeScore + giftScore;
+        }
+    }
+}
